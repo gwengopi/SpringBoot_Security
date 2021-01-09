fcf916b02ff9c63a49fbb536443ff3ec6bc91559
DIR\gopinath.palani <gopinath.palani@accenture.com>
2021-01-09 22:13:50 +0530

    WebsecurityConfigureAdapter - configure method with antMatchers

--- SpringSecurity/src/main/java/com/singtel/user/controller/UserLoginController.java
+++ SpringSecurity/src/main/java/com/singtel/user/controller/UserLoginController.java
@@ -2,6 +2,7 @@ package com.singtel.user.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class UserLoginController {
 	@GetMapping("/admin")
 	public String adminLogin() {
 		return "<h1>Admin login Success</h1>";
+	}
+	
+	@PreAuthorize("hasRole('admin')")
+	@PostMapping("/usercreate")
+	public String creatUser() {
+		return "<h1>User creation Success</h1>";
 	}
 }
--- SpringSecurity/src/main/java/com/singtel/user/controller/WebSecurityConfigurer.java
+++ SpringSecurity/src/main/java/com/singtel/user/controller/WebSecurityConfigurer.java
@@ -5,6 +5,7 @@ import javax.activation.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,8 +32,9 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 	  @Override 
 	  protected void configure(HttpSecurity http) throws Exception {
 	  
-	  http.authorizeRequests().
-	  antMatchers("/user").hasAnyRole("ADMIN"); 
+	  http.authorizeRequests().antMatchers(HttpMethod.POST).hasAnyRole("USER")
+	  .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+	  .antMatchers("/user").hasAnyRole("ADMIN"); 
 	  
 	  }
 	  @Bean public PasswordEncoder getPasswordEncoder() { return
